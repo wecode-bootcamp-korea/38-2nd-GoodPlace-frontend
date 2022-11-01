84778416c7d86445d96d33a9099fea5e644c81c0
Hyun Choi <hyun@Hyunui-MacBookAir.local>
2022-11-01 10:43:21 +0900

    NAV 레이아웃 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 const Main = () => {
-  return <div>메인페이지 입니다.</div>;
+  return <MainWrap>메인페이지 입니다.</MainWrap>;
 };
-
+const MainWrap = styled.div`
+  height: 1000px;
+`;
 export default Main;
