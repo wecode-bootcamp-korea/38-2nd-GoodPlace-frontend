41c9b996844168303acbbf5537541b2f68bc0d4a
Hyun Choi <hyun@Hyunui-MacBookAir.local>
2022-11-01 10:30:51 +0900

    MODIFY : 초기세팅 리뷰 수정사항 반영 완료 - singlQuote : true -> 팀원간의 convention을 doubleQuote로 하기로 해서 수정했습니다.

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,16 +2,16 @@ import { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
+  ${reset};
   *{
     box-sizing: border-box;
-  },
+  };
   button {
     cursor : pointer;
-  },
+  };
   input {
     outline : none;
-  }
+  };
 `;
 
 export default GlobalStyle;
